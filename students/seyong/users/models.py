ca36aa6461050be9e41a7319cffe38e225334d1a
se-yong <park0428@google.com>
2021-11-18 20:20:46 +0900

    model.py update2

--- students/seyong/users/models.py
+++ students/seyong/users/models.py
@@ -1,17 +1,18 @@
 from django.db import models
-# 이름, 이메일, 비밀번호, 연락처(휴대폰), 그 외 개인정보 
+
 class User(models.Model):
     name = models.CharField(max_length=20)
     email = models.CharField(max_length=45, unique=True)
     password = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=40, unique=True)
-    introduce = models.CharField(max_length=150)
-    gender = models.CharField(max_length=20, unique=True)
+    self_introduction = models.CharField(max_length=150)
+    gender = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
     class Meta:
         db_table = 'users'
 
 
 
-created_at = models.DateTimeField(auto_now_add=True)
-updated_at = models.DateTimeField(auto_now=True)
 
