8ad0f86e2f3ad7eaf24b917ece0e6f843af43fd7
dbwls314 <https://github.com/dbwls314>
2021-11-18 21:30:05 +0900

    models.py 수정1

--- students/youjin/westagram/users/models.py
+++ students/youjin/westagram/users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.db.models.fields import BooleanField
 # from django.db.models.fields import BooleanField
 
 class User(models.Model):
     name         = models.CharField(max_length=50)
-    email        = models.CharField(max_length=50)
+    email        = models.EmailField(max_length=200, unique=True, blank=True, null=True)
     passward     = models.CharField(max_length=100)
-    phone_number = models.IntegerField()
+    phone_number = models.PhoneNumberField(unique=True)
     gender       = models.CharField(max_length=50)
     sns          = models.BooleanField()
 
