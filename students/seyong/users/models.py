2462b339b02078967ba99511bbcc5b3bf548d052
se-yong <park0428@google.com>
2021-11-18 18:03:07 +0900

    models.py write

---
+++ students/seyong/users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+# 이름, 이메일, 비밀번호, 연락처(휴대폰), 그 외 개인정보 
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=45, unique=True)
+    password = models.CharField(max_length=200)
+    introduce = models.CharField(max_length=150)
+    gender = models.CharField(max_length=20, unique=True)
+    class Meta:
+        db_table = 'users'
+
+
+
+created_at = models.DateTimeField(auto_now_add=True)
+updated_at = models.DateTimeField(auto_now=True)
+
