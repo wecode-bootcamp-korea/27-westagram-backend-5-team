68b8138c58134083a9ab60457193e66f57f13a54
유현이 <yu@yuhyeon-iui-MacBookPro.local>
2021-11-18 18:42:55 +0900

    모델링 완료

---
+++ students/hyeoni/users/models.py
@@ -0,0 +1,13 @@
+#from django.db.models.fields import CharField
+from django.db import models
+
+class User(models.Model):
+    name        = models.CharField(max_length=100)
+    email       = models.CharField(max_length=100, unique=True)
+    password    = models.CharField(max_length=200)
+    phone_number= models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'users'
+
+
