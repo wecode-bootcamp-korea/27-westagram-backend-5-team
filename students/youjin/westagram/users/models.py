2c19d7982cd6f25416e17e2cd4bea9c35f6187a1
dbwls314 <https://github.com/dbwls314>
2021-11-19 15:23:27 +0900

    models.py 필드명 재수정

--- students/youjin/westagram/users/models.py
+++ students/youjin/westagram/users/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class User(models.Model):
     name         = models.CharField(max_length=50)
-    email        = models.EmailField(max_length=200, unique=True, blank=True, null=True)
+    email        = models.EmailField(max_length=200, unique=True)
     passward     = models.CharField(max_length=100)
     phone_number = models.PhoneNumberField(unique=True)
     gender       = models.CharField(max_length=50)
     sns          = models.BooleanField()
-    create_date  = models.DateTimeField(auto_now_add=True)
-    update_date  = models.DateTimeField(auto_now=True) 
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True) 
 
     class Meta:
         db_table = 'users'
