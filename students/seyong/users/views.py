567348d8d6327ba8909981ec7fd201bf4a6e40d0
se-yong <park0428@google.com>
2021-11-23 14:39:19 +0900

    wip

--- students/seyong/users/views.py
+++ students/seyong/users/views.py
@@ -5,7 +5,7 @@ from django.http  import JsonResponse
 from django.views import View
 from users.models  import User
 
-class SignupView(View):
+class SignUpView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
@@ -42,3 +42,25 @@ class SignupView(View):
 
         except KeyError:
             return JsonResponse({"MESSAGE": "KEY_ERROR"}, status = 400)
+
+class SignInView(View):
+    def post(self, request):
+        try:
+            data                  = json.loads(request.body)
+
+            email                 = data['email']
+            password              = data['password']
+
+            if not User.objects.filter(email = email).exists():
+                return JsonResponse({"MESSAGE": "INVALID_USER"}, status = 401)
+
+            if not User.objects.get(email = email).password == password:
+                return JsonResponse({"MESSAGE": "INVALID_USER"}, status = 401)
+
+            return JsonResponse({"MESSAGE": "SUCCESS"}, status = 200)
+
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status = 400)
+
+        except User.DoesNotExist:
+            return JsonResponse({"MESSAGE" : "INVALID_USER"}, status=401)
