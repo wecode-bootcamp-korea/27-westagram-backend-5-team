ab028967d348da8a9ffb105a5c4f0b21ffd5913d
이유진 <youjin@iyujin-ui-MacBookAir.local>
2021-11-23 21:01:27 +0900

    그냥

--- students/youjin/westagram/users/models.py
+++ students/youjin/westagram/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     name         = models.CharField(max_length=50)
     email        = models.EmailField(max_length=200, unique=True)
     passward     = models.CharField(max_length=100)
-    phone_number = models.PhoneNumberField(unique=True)
+    phone_number = models.CharField(max_length=100, unique=True)
     gender       = models.CharField(max_length=50)
     sns          = models.BooleanField()
     created_at   = models.DateTimeField(auto_now_add=True)
